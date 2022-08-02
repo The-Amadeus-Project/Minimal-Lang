0907a8eca39b3edbde634d64dcf99ae28fc127dd
CunningBard <jhonjhonjumaday@gmail.com>
2022-08-02 23:51:07 +0800

    tests

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+mod compiler;
 mod lexer;
+mod minimal_lang;
+mod parser;
+
+mod builder_dir {
+    pub mod builder;
+    pub mod return_code;
+}
 
 fn main() {
-    println!("Hello, world!");
+    minimal_lang::compile("main.min", true);
 }
