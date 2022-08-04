df52db5ca4b5bd60d87b07c07e25e85fb7b3d63e
CunningBard <jhonjhonjumaday@gmail.com>
2022-08-05 01:40:20 +0800

    transition to gas instead of nasm

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
+use std::fs;
+use std::fs::File;
+
 mod compiler;
 mod lexer;
 mod minimal_lang;
 mod parser;
 
 mod builder_dir {
-    pub mod builder;
+    pub mod builder_nasm;
     pub mod return_code;
+    pub mod builder_gas;
 }
 
 fn main() {
-    minimal_lang::compile("main.min", true);
+    let out_file = "out.asm";
+    let compiled = minimal_lang::compile("main.min", true);
+    let _res = File::create(out_file);
+    let _ = fs::write(out_file, compiled);
 }
